b26c0d987bb6949284422e24ad32d3d4fbe92b48
Johannes Bechberger <johannes.bechberger@sap.com>
2026-07-02 12:18:08 +0200

    fix(ci): set worker.format=es to fix Vite production build with code splitting

--- core/frontend/vite.config.ts
+++ core/frontend/vite.config.ts
@@ -28,6 +28,9 @@ export default defineConfig(({ mode }) => {
           } : {}),
         },
       },
+      worker: {
+        format: 'es',
+      },
       optimizeDeps: {
         exclude: ['@duckdb/duckdb-wasm'],
       },
