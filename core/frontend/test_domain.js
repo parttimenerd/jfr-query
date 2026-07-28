3740d1e1764b93af013acb701a0cee3e2a9d04f4
Johannes Bechberger <johannes.bechberger@sap.com>
2026-07-28 15:14:36 +0200

    chore: remove scratch test scripts from repo

--- core/frontend/test_domain.js
+++
@@ -1,6 +0,0 @@
-// The sub-token regex for DOMAIN
-const DOMAIN_PART = /DOMAIN\s+(\[[^\]]+\])/;
-
-console.log("Test 1 (normal):", DOMAIN_PART.test('DOMAIN [0, 100]'));
-console.log("Test 2 (empty):", DOMAIN_PART.test('DOMAIN []'));
-// DOMAIN [] should fail because [^\]]+ requires at least one non-] character
